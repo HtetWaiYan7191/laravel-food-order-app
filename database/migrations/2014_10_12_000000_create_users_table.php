11217cca01a44685e9a3566c1915bd6f15a7fca3
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-11-19 16:15:31 +0630

    Implement database and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('image', 20)->nullable();
+            $table->string('address')->nullable();
+            $table->string('role')->default('user');
+            $table->string('phone', 20)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
