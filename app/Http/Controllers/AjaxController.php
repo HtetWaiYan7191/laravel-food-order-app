dd8c1f19a1b1bb3d6eabc5577c5d54ed1cba368c
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-01 23:08:50 +0630

    Implement add to cart function in contorller

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -9,12 +10,29 @@ class AjaxController extends Controller
 {
     //
     public function pizzaList(Request $request) {
-        logger($request->all());
         if($request->status == 'asc') {
             $pizzas = Product::orderBy('created_at', 'asc')->get();
         } else {
             $pizzas = Product::orderBy('created_at', 'desc')->get();
         }
         return response()->json($pizzas, 200);
+    }
+
+    public function addToCart(Request $request) {
+        $data = $this->getData($request);
+        Cart::create($data);
+        $response = [
+            'message' => 'Item Added to the cart successfully ',
+            'status' => 'success'
+        ];
+        return response()->json($response, 200);
+    }
+
+    private function getData($request) {
+        return [
+            'user_id' => $request->userId,
+            'product_id'=> $request->pizzaId,
+            'quantity' => $request->count,
+        ];
     }
 }
